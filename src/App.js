11f25554d04d62e08d07ba0afad99361dfaea79c
Askary Abbas <dev.askary@gmail.com>
2022-06-11 18:28:40 +0530

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      nonsters: [
+        {
+          name: "Atul",
+        },
+        {
+          name: "Chhotu",
+        },
+        {
+          name: "Jacky",
+        },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>{this.state.monster1.name}</h1>
+        <h1>{this.state.monster2.name}</h1>
+        <h1>{this.state.monster3.name}</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
